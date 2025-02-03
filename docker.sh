1a7762d1edcaf9235fa142a15713457831f5b1dd
zevaryx <zevaryx@gmail.com>
2025-02-03 09:14:34 -0700

    Slightly smarter docker volume mounting

--- docker.sh
+++ docker.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker build --build-arg UID=$(id -u) --build-arg GID=$(id -g) -t scipy-k1 .
-docker run -v $PWD:/home/builder/scipy-k1 -it scipy-k1 bash -c "cd /home/builder/scipy-k1 && bash build.sh"
+docker build --build-arg UID=$(id -u) --build-arg GID=$(id -g) --build-arg UNAME=$USER -t scipy-k1 .
+docker run -v $PWD:$PWD -it scipy-k1 bash -c "cd $PWD && bash build.sh"
